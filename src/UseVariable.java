270e114c8a05a76d70cd5290bbd22155544c016d
kouichi.matsushita <dfhui4ty@yahoo.co.jp>
2023-11-11 23:26:38 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,14 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number2 = 200;
+        String str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+        int number3 = 2000;
+        System.out.println(number3);
+        
+    }
+
+}
